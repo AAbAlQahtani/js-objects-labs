3a46c8cc97efad64b7b189d21bbe68ac2b828a66
AAbAlQahtani <asmaalqhtani123@gmail.com>
2025-05-06 13:27:58 +0300

    add practice functions

---
+++ function.js
@@ -0,0 +1,14 @@
+
+const addNum = (num1,num2)=> num1+num2;
+const subNum = (num1,num2)=> num1-num2;
+const divNum = (num1,num2)=> num1/num2;
+const mulNum = (num1,num2)=> num1*num2;
+
+console.log("add 4+2 =")
+console.log(addNum(4,2))
+console.log("subtract 4-2 =")
+console.log(subNum(4,2))
+console.log("divide 4/2 =")
+console.log(divNum(4,2))
+console.log("multiply 4*2 =")
+console.log(mulNum(4,2))
---
+++ lab4.js
@@ -0,0 +1,8 @@
+saudiaAirlines=[{
+airlineName:"الخطوط الجوية السعودية",
+country: "السعودية",
+fleetSize:500,
+destinations:["الرياض", "جدة", "دبي", "نيويورك"],
+inOperation: true
+}
+]
