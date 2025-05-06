781cd3f47ee1ac97431b47dd026009f632cb3ec9
AAbAlQahtani <asmaalqhtani123@gmail.com>
2025-05-06 12:57:14 +0300

    add the question to lab1

--- main.js
+++ main.js
@@ -12,19 +12,21 @@ const book = {
         location: { city: 'Riyadh', country: 'SA' }
     }
 }
-
+console.log("- اطبع الكائن `Book`.")
 console.log(book);
+console.log("- اطبع الخاصية `title`.")
 console.log(book.title)
+console.log("- قم بتعديل قيمة الخاصية `year` إلى السنة الحالية واطبع الكائن.")
 book.year=new Date().getFullYear();
 console.log(book)
 
-
+console.log("- احذف الخاصية `author` واطبع الكائن.")
 delete book.author;
 console.log(book)
-
+console.log("- استدعِ الدالة `borrow` لتغيير `isAvailable` إلى false واطبع الكائن.")
 book.borrow();
 console.log(book);
-
+console.log("- اطبع الخاصية `publisher`.")
 console.log(book.publisher)
-
+console.log("- اطبع الخاصية `city` من داخل `location` obj .")
 console.log(book.publisher.location.city)
